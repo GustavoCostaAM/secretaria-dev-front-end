b4a0cef154450918f8d13071e538bdcb8eafc1a6
GustavoCostaAM <gustavo.costa.m949@gmail.com>
2026-02-26 21:15:45 -0300

    feature: add signin.js

---
+++ js/signin.js
@@ -0,0 +1,41 @@
+//seta elementos do input
+inputName = document.querySelector("#nome")
+inputUserName = document.querySelector("#username")
+inputEmail = document.querySelector("#email")
+inputPassword = document.querySelector("#password")
+
+//seta o botão de submit
+submitButton = document.querySelector("#submit")
+
+//adiciona evento de click ao botão de submit
+submitButton.addEventListener("click", async function(event) {
+    event.preventDefault() //previne o comportamento padrão do formulário
+
+    //cria um objeto com os dados do formulário
+    const userData = {
+        name: inputName.value,
+        username: inputUserName.value,
+        email: inputEmail.value,
+        password: inputPassword.value
+    }
+
+    //envia os dados para o backend
+        const response = await fetch(getLoginUrl(), {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+
+    //caso de sucesso na api
+    if (response.ok){
+        //pega o token retornado pela api
+            const token = await response.json().token
+
+        //salva o token no localStorage
+        localStorage.setItem("token", token)
+
+        window.location.href = "crud-base.html" //pagina principal do nosso crud
+    }
+})
---
+++ js/util.js
@@ -0,0 +1,5 @@
+const prodPath = "https://secretaria-dev-backend.onrender.com/"
+
+function getLoginUrl(){
+    return prodPath + "api/auth/login"
+}
