e30c1fb9351cd2deee599839dcbce93f7e75acb6
aoas2009 <aoasilva122009@hotmail.com>
2026-03-14 14:58:27 -0300

    style: mudanças no display dos popups

--- js/popup.js
+++ js/popup.js
@@ -6,4 +6,14 @@ function openDialog(id){
 function closeDialog(id){
     let dialog = document.getElementById(id)
     dialog.close()
+
+    //Caso for o popup de observações, limpar os inputs.
+    if(id="obs-popup"){
+        document.getElementById("popup-n1").value = undefined
+        document.getElementById("popup-n2").value = undefined
+        document.getElementById("popup-observacao").value = null
+
+        document.getElementById("popup-aluno").textContent = "Boletim de ..."
+    }
+
 }
