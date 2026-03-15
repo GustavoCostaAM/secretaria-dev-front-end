7f1674901d5186b8dfd60f49a492f8aa92d10868
aoas2009 <aoasilva122009@hotmail.com>
2026-03-15 18:57:48 -0300

    feat: adicionando popups de confirmação/erro para ações do crud

--- js/popup.js
+++ js/popup.js
@@ -17,3 +17,45 @@ function closeDialog(id){
     }
 
 }
+
+let i = 0;
+function showUpdatePopup(type, message){
+    console.log("Function called")
+    let popup = document.getElementById("update-popup");
+    let icon = document.getElementById("popup-icon");
+    let progressbar = document.getElementById("progress-bar")
+    let messageDiv = document.getElementById("popup-message")
+
+    popup.hidden = false
+    progressbar.style.width = "0%"
+    
+    if(type==="success"){
+        icon.src = "../assets/icons/fi-br-check.svg"
+        progressbar.style.backgroundColor = "var(--darker-green)"
+    }
+    else{
+        icon.src = "../assets/icons/fi-br-cross-red.svg"
+        progressbar.style.backgroundColor = "var(--red)"
+    }
+
+    messageDiv.textContent = message
+
+    let width = 1
+    let id = setInterval(frame, 25)
+    function frame() {
+        if(width >= 100){
+            clearInterval(id)
+            i = 0
+        } else {
+            width++
+            progressbar.style.width = width + "%";
+        }
+    }
+    setTimeout(()=>{
+        popup.hidden = true
+    }, 3000)
+    
+
+    console.log("Entrando na function")
+    // setInterval(updateProgressBar, 1)
+}
